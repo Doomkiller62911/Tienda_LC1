d4cdea39d2d4731e3f4db34e8c422c14d8fe3a66
DUM <106053263+Doomkiller62911@users.noreply.github.com>
2026-01-20 14:26:00 -0600

    It works

---
+++ src/main/java/tienda/tienda/ProyectConfig.java
@@ -0,0 +1,35 @@
+package tienda.tienda;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+/* Los siguiente métodos son para implementar el tema de seguridad dentro del proyecto */
+@Configuration
+public class ProyectConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/ejemplo2").setViewName("ejemplo2");
+        registry.addViewController("/multimedia").setViewName("multimedia");
+        registry.addViewController("/iframes").setViewName("iframes");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
+    }
+
+    /* El siguiente método se utilizar para publicar en la nube, independientemente */
+    @Bean
+    public SpringResourceTemplateResolver templateResolver0() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    }
+}
