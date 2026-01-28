86ccdf1cca3b30e53c4316f6d62472e9424771d2
DUM <106053263+Doomkiller62911@users.noreply.github.com>
2026-01-27 20:17:44 -0600

    Semana 3 terminado

---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.tienda.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tienda.domain.Categoria;
+import com.tienda.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activo) {
+        if (activo) {
+            return categoriaRepository.findByActivoTrue();
+
+        }
+        return categoriaRepository.findAll();
+    }
+}
